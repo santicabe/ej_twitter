92f960587220466364ac0d085cae04f4a1ded361
santicabe <santiago.cabe@gmail.com>
2021-04-22 12:22:54 -0300

    Create a tweet!!

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 const { render } = require("ejs");
 const express = require("express");
 const router = express.Router();
-
 const publicController = require("../controllers/publicController");
+const userController = require("../controllers/userController");
 const loginControl = require("../controllers/loginController");
 
 router.get("/", (req, res) => res.render("login", {}));
@@ -13,5 +13,6 @@ router.get("/register", (req, res) => res.render("register", {}));
 router.post("/register", publicController.sendRegister);
 
 router.get("/home", (req, res) => res.render("home", {}));
+router.post("/home", userController.tweetCreate);
 
 module.exports = router;
